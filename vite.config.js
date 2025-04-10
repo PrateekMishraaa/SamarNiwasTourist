984549a2e9d976c85ea9172869ab9a84ebf9111a
Prateek Mishra <pm921670@gmail.com>
2025-04-10 11:01:23 +0530

    minor-changes

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,16 @@
 import { defineConfig } from 'vite'
-import tailwindcss from '@tailwindcss/vite'
+import react from '@vitejs/plugin-react'
+import { createHtmlPlugin } from 'vite-plugin-html'
+
 export default defineConfig({
   plugins: [
-    tailwindcss(),
-  ],
+    react(),
+    createHtmlPlugin({
+      inject: {
+        data: {
+          title: 'Samar Niwas Dehradun | Comfortable Stays & Special Discounts'
+        }
+      }
+    })
+  ]
 })
